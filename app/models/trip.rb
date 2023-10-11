778a771ed3f4cffb8dd575ab4a6e1b655b5b1eb3
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-10-11 10:49:25 -0500

    attempted strftime

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,13 @@
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :places
+
+  # def friendly_start_time
+  #   start_time.strftime("%D")
+  # end
+
+  # def friendly_end_time
+  #   end_time.strftime("%D")
+  # end
+  #model method strftime
 end
--- app/views/trips/_trip.json.jbuilder
+++ app/views/trips/_trip.json.jbuilder
@@ -4,3 +4,6 @@ json.image_url trip.image_url
 json.start_time trip.start_time
 json.end_time trip.end_time
 json.places trip.places
+json.start_time trip.start_time
+json.end_time trip.end_time
+
